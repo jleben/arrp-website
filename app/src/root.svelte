49966ef66a5ae5274451a209a642b99b980d7399
Jakob Leben <jakob.leben@gmail.com>
2020-05-28 11:54:54 -0700

    Router component: Enable declarative route definition in HTML

--- app/src/root.svelte
+++ app/src/root.svelte
@@ -1,29 +1,20 @@
 <svelte:options tag="x-root" />
 
-<h1>Arrp</h1>
+<h1><a href='/'>Arrp</a></h1>
 <a href='/doc/v1.0.0'>Documentation</a>
 <a href='/play'>Playground</a>
+<a href='/unknown'>Unknown</a>
 
-<router-view id='router' bind:this={router}></router-view>
+<router-view id='router'>
+  <router-route path='/' component='x-home'></router-route>
+  <router-route path='/doc/v1.0.0' component='x-doc-1-0-0'></router-route>
+  <router-route path='/play' component='x-play'></router-route>
+  <router-route path='(.*)' component='x-home'></router-route>
+</router-view>
 
 <script>
 import './router/router.js'
 import './home.svelte'
 import './doc/doc_v1_0_0.svelte'
 import './play/play.svelte'
-import { onMount } from 'svelte';
-
-let router;
-
-onMount(() => {
-  let routes = [
-    {path: '/',     component: 'x-home'},
-    {path: '/doc/v1.0.0',  component: 'x-doc-1-0-0'},
-    {path: '/play', component: 'x-play'},
-    {path: '(.*)', component: 'x-home'},
-  ]
-
-  router.setRoutes(routes);
-})
-
 </script>
--- app/src/router/router.js
+++ app/src/router/router.js
@@ -1,17 +1,61 @@
 import {Router} from '@vaadin/router'
 
 export class RouterView extends HTMLElement {
+
   constructor() {
     super();
 
     let shadow = this.attachShadow({ mode: 'open' });
 
     this.router = new Router(shadow);
+    this.routeTimer = null;
   }
 
   setRoutes(routes) {
     this.router.setRoutes(routes);
   }
+
+  requestUpdateRoutesFromChildren() {
+    if (this.routeTimer) {
+      return;
+    }
+
+    this.routeTimer = setTimeout(() => this.updateRoutesFromChildren());
+  }
+
+  updateRoutesFromChildren() {
+    this.routeTimer = null;
+
+    let routes = [];
+
+    for (let child of this.childNodes) {
+      if (child instanceof Route) {
+        routes.push({
+          path: child.getAttribute('path'),
+          component: child.getAttribute('component')
+        });
+      }
+    }
+
+    this.router.setRoutes(routes);
+  }
+}
+
+export class Route extends HTMLElement {
+
+
+    connectedCallback() { this.requestRouteUpdate();  }
+    disconnectedCallback() { this.requestRouteUpdate();  }
+    attributeChangedCallback() { this.requestRouteUpdate(); }
+
+    requestRouteUpdate() {
+      const view = this.parentElement;
+      if (view && view instanceof RouterView)
+      {
+          view.requestUpdateRoutesFromChildren();
+      }
+    }
 }
 
 customElements.define('router-view', RouterView);
+customElements.define('router-route', Route);

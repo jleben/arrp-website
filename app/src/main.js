ec5dbfa70ae43a9f79d97f8432c1a65f58de7412
Jakob Leben <jakob.leben@gmail.com>
2020-06-01 21:03:56 -0700

    app/doc/v1/index

--- app/src/main.js
+++ app/src/main.js
@@ -5,6 +5,7 @@ import home_page from '../build/home.md-html'
 import './root.svelte'
 import './play/play.svelte'
 import './doc/doc_v1_0_0.svelte'
+import doc_v1_index from '../build/doc/v1.0.0/index.md-html'
 
 const router = new Router(document.body);
 
@@ -14,7 +15,7 @@ router.setRoutes([
     component: 'x-root',
     children: [
       { path: '/play', component: 'x-play' },
-      { path: '/doc/v1.0.0', component: 'x-doc-1-0-0' },
+      { path: '/doc/v1.0.0', action: fetchHtmlAction(doc_v1_index) },
       { path: '(.*)', action : fetchHtmlAction(home_page) },
     ]
   }

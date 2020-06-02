b9e938d5dbce8fa0f47ed6915397faf63896d83c
Jakob Leben <jakob.leben@gmail.com>
2020-06-01 20:51:10 -0700

    app/root: A little styling

--- app/src/root.svelte
+++ app/src/root.svelte
@@ -1,8 +1,23 @@
 <svelte:options tag="x-root" />
 
+<div id='header'>
 <h1><a href='/'>Arrp</a></h1>
 <a href='/doc/v1.0.0'>Documentation</a>
 <a href='/play'>Playground</a>
-<a href='/unknown'>Unknown</a>
+</div>
 
 <slot></slot>
+
+<style>
+a {
+  text-decoration: none;
+  color: #ffffff;
+}
+
+#header {
+  background-color: #000066;
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+}
+</style>
